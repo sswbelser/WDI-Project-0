4faf8aa3c4b2f1c32d9af84fb6ffd1974daa6254
Sabastian <sswbelser@gmail.com>
2015-07-03 14:25:06 -0700

    Changes to scripts. Still no submit functionality; saving to local storage works

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// THIS WORKS WHEN SAVING TO LOCAL STORAGE, but not submit button
 var Post = function(image, title, body, ratings, comments) {
 	this.image = image;
 	this.title = title;
@@ -37,3 +38,65 @@ var newPost = new Post ("http://images.akamai.steamusercontent.com/ugc/882976651
 newPost.saveToLs(newPost);
 
 newPost.renderTemplate("#post-template", "#post-container");
+
+// $(function() {
+// var $newPost = $("#new-post");
+// // var $newComment = $("#new-comment");
+// var $postList = $("#post-list");
+// var postTemplate = _.template($("post-template").html());
+
+// var Post = function(image, title, body) {
+// 	this.image = image;
+// 	this.title = title;
+// 	this.body = body;
+// 	// this.ratings = ratings;
+// 	// this.comments = comments;
+
+// 	// this.posts = localStorage.getItem("posts");
+// 	// this.key = "posts";
+// }
+// Post.all = [];
+
+// Post.prototype.save = function(postImage, postTitle, postBody) {
+// 	Post.all.push(this);
+// }
+
+// Post.prototype.render = function(postImage, postTitle, postBody) {
+// 	var index = Post.all.indexOf(this);
+// 	var $post = $(postTemplate(this));
+// 	$post.attr("data-index", index);
+// 	$postList.append($post);
+// }
+
+// $newPost.on("submit", function() {
+// 	event.preventDefault();
+// 	var postImage = $("#new-image").val();
+// 	var postTitle = $("#new-title").val();
+// 	var postBody = $("#new-body").val();
+// 	// var postRating = $("#rating").val();
+// 	// var postComment = $("#comment").val();
+// 	var newPost = new Post(postImage, postTitle, postBody);
+
+// 	newPost.save();
+// 	newPost.render();
+
+// 	console.log(Post.all);
+
+// 	// $newPost[0].reset();
+// });
+
+// });
+
+// $newComment.on("submit", function() {
+// 	event.preventDefault();
+// 	var commentRating = $("#rating").val();
+// 	var commentComment = $("#comment").val();
+// 	var newComment = new Post(commentRatings, commentComments);
+
+// 	newComment.save();
+// 	newComment.render();
+
+// 	console.log(Post.all);
+
+// 	$newPost[0].reset();
+// });
