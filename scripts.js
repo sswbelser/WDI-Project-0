6a1ad2b83ceeb247744e425a00aff981a1666e4c
Sabastian <sswbelser@gmail.com>
2015-07-05 16:48:18 -0700

    multiple fixes; comments mostly work

--- scripts.js
+++ scripts.js
@@ -1,30 +1,50 @@
 $(function() {
 var $newPost = $("#modal-form");
-// var $newComment = $("#new-comment");
+var $newComment = $("#comment-modal-form");
 var $postList = $("#post-list");
+var $commentList = $("#comment-list");
 var postTemplate = _.template($("#post-template").html());
+var commentTemplate = _.template($("#comment-template").html());
+var totalPosts = 0
 
 var Post = function(image, title, body) {
 	this.image = image;
 	this.title = title;
 	this.body = body;
-	// this.ratings = ratings;
-	// this.comments = comments;
 
 	// this.posts = localStorage.getItem("posts");
 	// this.key = "posts";
 }
+
+var Comment = function(rating, comment) {
+	this.rating = rating;
+	this.comment = comment;
+}
+
 Post.all = [];
+
+Comment.all = [];
 
 Post.prototype.save = function() {
 	Post.all.push(this);
 }
 
+Comment.prototype.save = function() {
+	Comment.all.push(this);
+}
+
 Post.prototype.render = function() {
-	var index = Post.all.indexOf(this);
+	var postIndex = Post.all.indexOf(this);
 	var $post = $(postTemplate(this));
-	$post.attr("data-index", index);
+	$post.attr("data-index", postIndex);
 	$postList.append($post);
+}
+
+Comment.prototype.render = function() {
+	var commentIndex = Comment.all.indexOf(this);
+	var $comment = $(commentTemplate(this));
+	$comment.attr("data-index", commentIndex);
+	$commentList.append($comment);
 }
 
 $newPost.on("submit", function() {
@@ -32,8 +52,6 @@ $newPost.on("submit", function() {
 	var postImage = $("#new-image").val();
 	var postTitle = $("#new-title").val();
 	var postBody = $("#new-body").val();
-	// var postRating = $("#rating").val();
-	// var postComment = $("#comment").val();
 	var newPost = new Post(postImage, postTitle, postBody);
 
 	newPost.save();
@@ -44,18 +62,18 @@ $newPost.on("submit", function() {
 	$newPost[0].reset();
 });
 
+$newComment.on("submit", function () {
+	event.preventDefault();
+	var commentRating = $("#new-rating").val();
+	var commentComment = $("new-comment").val();
+	var newComment = new Comment(commentRating, commentComment);
+
+	newComment.save();
+	newComment.render();
+
+	console.log(Comment.all);
+
+	$newComment[0].reset();
 });
 
-// $newComment.on("submit", function() {
-// 	event.preventDefault();
-// 	var commentRating = $("#rating").val();
-// 	var commentComment = $("#comment").val();
-// 	var newComment = new Post(commentRatings, commentComments);
-
-// 	newComment.save();
-// 	newComment.render();
-
-// 	console.log(Post.all);
-
-// 	$newPost[0].reset();
-// });
+});
